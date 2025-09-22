46791f8dec703ea08e1de98806cc29ac9009d91c
SwapniL1563 <choratswapnil15@gmail.com>
2025-09-22 16:21:09 +0530

    add orbit-control

---
+++ 05-OrbitControls/src/script.js
@@ -0,0 +1,79 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js"
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+const event = {
+    x:0,
+    y:0
+}
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+scene.add(mesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.x = 2;
+camera.position.y = 2;
+camera.position.z = 2;
+camera.lookAt(mesh.position);
+scene.add(camera);
+
+const controls = new OrbitControls(camera,canvas);
+controls.enableDamping = true;
+
+
+
+// window.addEventListener("mousemove",(e)=> {
+//   event.x = e.clientX / sizes.width - 0.5
+//   event.y = -1 * (e.clientY / sizes.height - 0.5)
+//   console.log(event.x,event.y);
+  
+// });
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+// Animate
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+  // mesh.rotation.y = elapsedTime;
+
+  // camera.position.x = Math.sin(event.x * Math.PI * 2) * 2;
+  // camera.position.z = Math.cos(event.x * Math.PI * 2) * 2;
+  // camera.position.y = event.y * 3;
+  // camera.lookAt(mesh.position);
+  
+  controls.update();
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
